91cab1f30f2a730de2997a2f439ef934bbbf6fda
Alex Castellar <acaste42@calpoly.edu>
2025-06-02 12:27:59 -0700

    Initial radio driver project

--- code/ShiftRegisters/Core/Inc/shiftreg.h
+++ code/ShiftRegisters/Core/Inc/shiftreg.h
@@ -4,23 +4,34 @@
  * Created by Alex Castellar
 */
 
-/** Circuit and IOC Setup
+/** Circuit, IOC and Configuration Setup
  * In the circuit
  * 	The TPIC6595 requires 5V but STM32 GPIOs can only drive 3.3V
  * 	Thus, the GPIOs must be Open Drain with an external 5V pull-up
- * 	DO NOT USE a 150 ohm pullup, 33 mA is too much for the GPIO drivers
+ * 	A 1k pull-up is probably a good number
+ *
+ * It is assumed that the TPIC6595s are daisy chained and properly connected
  *
  * In the GPIO configuration:
  * 	set GPIO mode to Output Open Drain
  * 	set Maximum Output Speed to Very High (if possible)
  *
- * Ensure you set SHIFTREG_*:
- *  SHIFTREG_*_GPIO_EXPANDER to the expander being used (GPIOX)
- *  SHIFTREG_*_GPIO_PIN to the pin number used (GPIO_PIN_X)
- * 	SHIFTREG_*_SPEED_SETTING to the set Maximum Output Speed
- * 	SHIFTREG_*_PULLUP_OHMS to the external 5V pullup ohm value
+ * Ensure you set SHIFTREG_* in shiftreg_conf.h:
+ *  _GPIO_EXPANDER to the expander being used (GPIOX)
+ *  _GPIO_PIN to the pin number used (GPIO_PIN_X)
+ *  _SPEED_SETTING to the set Maximum Output Speed
+ * 	_PULLUP_OHMS to the external 5V pullup ohm value
+ * 	_CAPACITANCE_PICOFARADS to estimate of capacitance in the circuit
  *
- * This assumes that the TPIC6595s are daisy chained and properly connected
+ */
+
+/* ****** CAUTION ******
+ *
+ * DO NOT USE a 150 ohm pullup, 33 mA is too much for the GPIO drivers
+ * The pins used MUST BE 5V TOLERANT (FT)
+ *  in the stm32l476rg reference manual, Table 16 (STM32L476xx pin definitions):
+ * 	NOT 5V TOLERANCE: PA3, PA4, PA5, PB0
+ * 	All other GPIOs on the LQFP64 package are FT
  *
  */
 
